b342d3f27f62aedbdf5d1e8e77729c237a5f0dbf
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-13 00:03:12 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
-DB_URI = os.environ.get("DATABASE_URL", "")
+TOKEN = os.environ.get("TOKEN", "6315773312:AAFT7zTTg6yP1807xqbCcfIChnWKDOU3aas") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "iam_daxx") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "HEROKUFREECC") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "ALLTYPECC") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
