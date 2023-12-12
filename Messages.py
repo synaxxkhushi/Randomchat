7857e40d761d494362b74e4a7abed7a2724b7e70
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-12 23:49:55 +0530

    Add files via upload

---
+++ Messages.py
@@ -0,0 +1,37 @@
+m_start = "⚡️ WELCOME ⚡️\n\n I hope you can make a friend or soul mate\n\n I hope you can make a friend or a partner\n\n💬 to find someone to chat with, use the New Chat button"
+
+ m_is_not_free_users = (
+     "❗️ Sorry you are not in chat\n\n❗️ Sorry you are not in chat"
+ )
+
+ m_is_connect = "✅ You are already in the chat, please send your chat\n\n✅ You are already in the chat, please send your chat"
+
+ m_play_again = "Do you want to chat with other people?\n\nDo you want to chat with other people?"
+
+ m_is_not_user_name = "❌ Sorry, our bot can only communicate if you have a username\n\n❌ Sorry, our bot can only communicate if you have a username"
+
+ m_good_bye = "❌ Sorry your interlocutor left the chat\n\n❌ Sorry your interlocutor left the chat"
+
+ m_disconnect_user = "Sorry the connection was lost!\n\nSorry, the connection was lost!"
+
+ m_failed = "❌ An error occurred!\n\n❌ Loss happened"
+
+ m_like = "✅ You sent the username to the other person!\n\n✅ You sent the username to the other person"
+
+ m_dislike_user = "Chat Has Ended\n\nChat Has Ended"
+
+ m_dislike_user_to = (
+     "⛔ Your interlocutor leaves the chat\n\n⛔ Your interlocutor leaves the chat"
+ )
+
+ m_send_some_messages = "The bot could not forward messages from the bot\n\nThe bot could not continue the pan from the bot"
+
+ m_has_not_dialog = "You are not in chat\n\nYou are not in chat"
+
+ dislike_str = "👎 Doesn't like you"
+
+ like_str = "👍 Like you"
+
+
+ def m_all_like(x):
+     return "Here sis " + "username : @" + str(x)
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "6315773312:AAFT7zTTg6yP1807xqbCcfIChnWKDOU3aas") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "iam_daxx") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "HEROKUFREECC") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "ALLTYPECC") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
