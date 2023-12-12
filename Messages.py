4a643374c211ef2b78a7fe9d56e235b6170c4bcf
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-13 00:24:17 +0530

    Update Messages.py

--- Messages.py
+++ Messages.py
@@ -1,7 +1,7 @@
 m_start = "⚡️ SELAMAT DATANG ⚡️\n\n🇮🇩 Semoga Dapat teman atau jodoh\n\n🇳🇿 I hope you can make a friend or a partner\n\n💬 untuk mencari teman obrolan gunakan button New Chat"
 
 m_is_not_free_users = (
-    "❗️ Maaf Anda tidak berada dalam obrolan\n\n❗️ Sorry you are not in chat"
+    "🥺 Sᴏʀʀʏ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ ᴄʜᴀᴛ \n\n☹️ Sᴏʀʀʏ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ ᴄʜᴀᴛ"
 )
 
 m_is_connect = "✅ Anda sudah berada di dalam obrolan, silahkan kirim chat anda\n\n✅ You are already in the chat, please send your chat"
@@ -18,7 +18,7 @@ m_failed = "❌ Terjadi kesalahan!\n\n❌ Loss happened"
 
 m_like = "✅ Anda mengerimkan username kepada lawan bicara!\n\n✅ You send the username to the other person"
 
-m_dislike_user = "Obrolan Telah Berakhir\n\nChat Has Ended"
+m_dislike_user = "Cʜᴀᴛ Hᴀs Eɴᴅᴇᴅ💔 \n\n Cʜᴀᴛ Hᴀs Eɴᴅᴇᴅ💔"
 
 m_dislike_user_to = (
     "⛔ Lawan bicara anda meninggalkan obrolan\n\n⛔ Your interlocutor leaves the chat"
@@ -26,11 +26,11 @@ m_dislike_user_to = (
 
 m_send_some_messages = "Bot tidak bisa meneruskan pesan dari bot\n\nThe bot could not continue the pan from the bot"
 
-m_has_not_dialog = "Anda tidak sedang dalam obrolan\n\nYou are not in chat"
+m_has_not_dialog = "Yᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ ᴄʜᴀᴛ\n\nYᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ ᴄʜᴀᴛ"
 
-dislike_str = "👎 Tidak menyukai kamu"
+dislike_str = "𝗥𝗲𝗷𝗲𝗰𝘁𝗲𝗱❌"
 
-like_str = "👍 Menyukai kamu"
+like_str = "𝗔𝗰𝗰𝗲𝗽𝘁✅"
 
 
 def m_all_like(x):
